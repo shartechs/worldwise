403b2b4c27490d42a0ee10d5324be4d8e7a23c51
Shartechs <shartechs@gmail.com>
2023-11-04 19:47:32 +0400

    adding components and style modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import PageNotFound from "./pages/PageNotFound";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 
 import "./App.css";
 
@@ -10,10 +11,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,10 @@
+import AppNav from "./components/AppNav.jsx";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
