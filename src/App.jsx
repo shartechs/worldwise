5dc9f7857a8f9fe9985683e8c5edf3c5ca03eb69
Shartechs <shartechs@gmail.com>
2023-11-08 15:32:32 +0400

    Adding sidebar tab navigation with nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,15 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index path="cities" element={<p>cities</p>} />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="/" element={<Homepage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
